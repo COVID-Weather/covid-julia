28cda86ae42dc6f07a80acce59aebe695f60bc44
Gregory Wagner <wagner.greg@gmail.com>
2020-04-01 16:12:01 -0400

    Updates

--- weath_distribution.jl
+++ weath_distribution.jl
@@ -1,69 +1,60 @@
-using Agents, Random, Distributions
+# # Wealth distribution model
 
-mutable struct Human <: AbstractAgent
-           id :: Int
-       status :: Symbol # :susceptible, :infected, or :removed
-    infection :: Float64
+# This model is a simple agent-based economy that is modelled according
+# to the work of [Dragulescu *et al.*](https://arxiv.org/abs/cond-mat/0211175).
+# This work introduces statistical mechanics concepts to study wealth distributions.
+# For this reason what we show here is also referred to as "Boltzmann wealth
+# distribution" model.
+
+# This model has a version with and without space.
+# The rules of the space-less game are quite simple:
+# 1. There is a pre-determined number of agents.
+# 2. All agents start with one unit of wealth.
+# 3. At every step an agent gives 1 unit of wealth (if they have it) to some other agent.
+
+# Even though these are some very simple rules, they can still create the basic
+# properties of wealth distributions, e.g. power-laws distributions.
+
+# ## Core structures, space-less
+# We start by defining the Agent type and initializing the model.
+using Agents
+mutable struct WealthAgent <: AbstractAgent
+    id::Int
+    wealth::Int
 end
 
-Random.seed!(123)
+# Notice that this agent does not have a `pos` field. That is okay, because
+# there is no space structure to this example.
+# We can also make a very simple [`AgentBasedModel`](@ref) for our model.
 
-function SIRModel(;
-                   humans = 1000,         # Number of susceptible
-                   infected = 10,           # Number of infected
-                   contact_rate = 0.1,
-                   probability_of_infection = 0.1,
-                   initial_infection = Uniform(0, 1) # Probability distribution for time-since-infected
-                  )
-
-    model_properties = Dict(
-                            :contact_rate             => contact_rate,
-                            :probability_of_infection => probability_of_infectiocontact_rate,
-                            :total                    => humans,
-                            :infected                 => infected,
-                           )
-
-    model = ABM(Human, scheduler=fastest, properties=model_properties)
-
-    # Initialize susceptible population
-    susceptible = humans - infected
-
-    for i = 1:susceptible
-        add_agent!(model, :susceptible, 0.0)
+function wealth_model(;numagents = 100, initwealth = 1)
+    model = ABM(WealthAgent, scheduler=random_activation)
+    for i in 1:numagents
+        add_agent!(model, initwealth)
     end
-
-    # Initialize infected population
-    for i = 1:infected
-        add_agent!(model, :infected, rand(initial_infection))
-    end
-
     return model
 end
 
-function agent_step!(agent, m)
+model = wealth_model()
 
-    contacted = rand(Poisson(m.infected, m.contact_rate))
-
-    if status == :removed
-        return nothing
-    elseif status  == :susceptible
-        # agent may become infected
-    elseif status == :infected
-        # agent may become removed
-    end
-
-    return nothing
+# The next step is to define the agent step function
+function agent_step!(agent, model)
+    agent.wealth == 0 && return # do nothing
+    ragent = random_agent(model)
+    agent.wealth -= 1
+    ragent.wealth += 1
 end
 
+# We use `random_agent` as a convenient way to just grab a random agent.
+# (this may return the same agent as `agent`, but we don't care in the long run)
+
+# ## Running the space-less model
+# Let's do some data collection, running a large model for a lot of time
 N = 5
 M = 2000
-
 agent_properties = [:wealth]
-
 model = wealth_model(numagents=M)
-
 data = step!(model, agent_step!, N, agent_properties)
-
 data[end-20:end, :]
 
 # What we mostly care about is the distribution of wealth,
@@ -75,7 +66,6 @@ wealths = filter(x -> x.step == N, data)[!, :wealth]
 # With a simple visualization we immediatelly see the power-law distribution:
 
 using UnicodePlots
-
 UnicodePlots.histogram(wealths)
 
 # ## Core structures, with space
@@ -90,14 +80,12 @@ mutable struct WealthInSpace <: AbstractAgent
 end
 
 function wealth_model_2D(;dims = (25,25), wealth = 1, M = 1000)
-    space = GridSpace(dims, periodic = true)
-    model = ABM(WealthInSpace, space; scheduler = random_activation)
-
-    for i in 1:M # add agents in random nodes
-        add_agent!(model, wealth)
-    end
-
-    return model
+  space = GridSpace(dims, periodic = true)
+  model = ABM(WealthInSpace, space; scheduler = random_activation)
+  for i in 1:M # add agents in random nodes
+      add_agent!(model, wealth)
+  end
+  return model
 end
 
 model2D = wealth_model_2D()
